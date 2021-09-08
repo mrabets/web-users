05618d874448bc28fdea1cc8e6593f2669eef1e3
mrabets <newrabgo@gmail.com>
2021-09-08 21:41:52 +0300

    Add github and google to devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,6 +273,17 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  # Add for Facebook here
+  # ...
+
+  github_client_id = Rails.application.credentials.github[:client_id]
+  github_client_secret = Rails.application.credentials.github[:client_secret]
+  config.omniauth :github, github_client_id, github_client_secret, scope: 'user,public_repo'
+
+  google_client_id = Rails.application.credentials.google_oauth2[:client_id]
+  google_client_secret = Rails.application.credentials.google_oauth2[:client_secret]
+  config.omniauth :google_oauth2, google_client_id, google_client_secret, scope: 'userinfo.email,userinfo.profile', skip_jwt: true
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
