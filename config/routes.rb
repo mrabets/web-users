db07a0152de4292362f137f9c0ccfe407389b5de
mrabets <newrabgo@gmail.com>
2021-09-11 10:48:11 +0300

    Create table with sort

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'home#index'
-
-  get '/list', to: 'pages#list'
+  match '/users/:id',     to: 'users#show',       via: 'get'
+  resources :users, :only =>[:show]
+  get '/list', to:   'pages#list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
